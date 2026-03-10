f6cc94deffc77b7afd3e4389998632d278b82c95
bonbon-on-fire <nishita@bhakars.com>
2026-03-10 10:11:14 -0400

    feat: ran more clueless simulations

--- terminal_ui/run_batch.py
+++ terminal_ui/run_batch.py
@@ -43,7 +43,7 @@ STUDENT_PERSONAS: list[str] = ["clueless_03"]
 
 # Which course/exercise combinations to run.
 # Exercise numbers should be zero-padded strings like "01".
-COURSE_EXERCISES: list[tuple[str, str]] = [("philosophy", "01")]
+COURSE_EXERCISES: list[tuple[str, str]] = [("urban_studies", "02")]
 
 # Turn size per conversation (student+tutor exchanges).
 TURN_SIZE: int = 10
